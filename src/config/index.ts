c9548f86c126c5d4c0822dcb6ae0be4067ab93f6
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-04-22 20:03:00 -0300

    configuring dynamic sdk provider

---
+++ src/config/index.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+const api = {
+  url: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+};
+
+const app = {
+  url: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3001',
+  title: process.env.NEXT_PUBLIC_APP_TITLE || 'Ventura | Your finance service',
+};
+
+const dynamic = {
+  environmentId: process.env.NEXT_PUBLIC_APP_ENVIRONMENT_ID || ''
+}
+
+const environment = process.env.NODE_ENV;
+
+export default {
+  api,
+  app,
+  dynamic,
+  environment,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header from '@/components/layout/header';
+import config from '@/config';
+import '@/styles/globals.css';
+import { DynamicContextProvider } from '@dynamic-labs/sdk-react';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DynamicContextProvider
+        settings={{
+          appLogoUrl:
+            'https://upload.wikimedia.org/wikipedia/commons/3/34/Examplelogo.svg',
+          appName: 'Vencura',
+          environmentId: config.dynamic.environmentId,
+          multiWallet: true,
+        }}
+      >
+        <Header /> 
+        <Component {...pageProps} />
+      </DynamicContextProvider>
+    </>
+  );
 }
